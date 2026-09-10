0c88bf75485ca1dd75c13fcf092b127a44061801
Hannower Monteiro <hannowersousa@gmail.com>
2026-09-10 18:29:40 -0300

    feat/navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
+import './Navbar.css';
+
 export default function Navbar() {
-    return(
-        <>
-            <h1>Nav Bar</h1>
-        </>
-    )
+  const [menuAberto, setMenuAberto] = useState(false);  
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">&lt;Hannower Monteiro /&gt;</div>
+
+      <ul className={`navbar-menu ${menuAberto ? 'aberto' : ''}`}>
+        <li><a href="#projetos" onClick={() => setMenuAberto(false)}>Projetos</a></li>
+        <li><a href="#sobre" onClick={() => setMenuAberto(false)}>Sobre</a></li>
+        <li><a href="#contato" onClick={() => setMenuAberto(false)}>Contato</a></li>
+      </ul>
+
+      <button
+        className={`navbar-hamburguer ${menuAberto ? 'aberto' : ''}`}
+        onClick={() => setMenuAberto(!menuAberto)}
+        aria-label="Abrir menu"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </nav>
+  );
 }
